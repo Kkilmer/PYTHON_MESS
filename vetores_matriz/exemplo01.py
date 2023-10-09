db1f77bd2d7bbc5aa5e09c390a453bd6377fcb89
Kkilmer <kp827767@gmail.com>
2023-10-09 20:54:40 -0300

    py_aula09

---
+++ vetores_matriz/exemplo01.py
@@ -0,0 +1,17 @@
+# Implementamos uma lista em Python
+#lista_de_jogos = ['Super Mario',
+#                'Sonic','Pokemon Go']
+
+# Implementamos uma lista de listas em Python
+#matriz_de_numeros = [[1, 2, 3],
+#                    [4, 5, 6],
+#                    [7, 8, 9]]
+
+lista_de_series = [
+    'Quantico',
+    'The Chosen',
+    'Loki'
+]
+
+for serie in lista_de_series:
+    print(f'A melhor serie é {serie.lower()}')
---
+++ vetores_matriz/exemplo02.py
@@ -0,0 +1,10 @@
+#lista de contatos
+contatos = [
+    ['Mãe','(83)99999999', '19/04/1981'],
+    ['Polícia','190'],
+    ['SAMU', '192'],
+    ['Monaci','839888', '18/07/1999']
+]
+
+for contato in contatos:
+    print(f'Nome: {contato[0]} | Telefone: {contato[1]}')
