68cf35465cc7b1f06fc2c9e28691d26c4f0d157f
Kkilmer <kp827767@gmail.com>
2023-10-30 21:33:40 -0300

    Modelo aula

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.run()
+# definindo a variavel de ambiente
+os.environ['FLASK_DEBUG'] = 'True'
+
+# configurando o modo de depuracao com base..
+app.debug = os.environ.get('FLASK_DEBUG') == 'True'
+
+
+@app.route('/')
+def ola():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
